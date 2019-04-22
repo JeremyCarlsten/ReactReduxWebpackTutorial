4294705aa5b071759199ae4a3c25aa32ba4e5e64
JeremyCarlsten <jeremy@jeremycarlsten.com>
2019-04-22 12:26:48 -0500

    getting the routing working

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from '../store'
+import { history } from '../store/history';
 import { ConnectedDashboard } from './Dashboard';
+import { Router, Route} from 'react-router-dom';
 
 export const Main = () => (
-    <Provider store={store}>
-        <ConnectedDashboard />
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <Route exact path='/dashboard' render={ () => (<ConnectedDashboard />)} />
+        </Provider>
+    </Router>
 ); 
